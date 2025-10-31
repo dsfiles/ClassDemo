a7b28edd702694f0be85ef8825918c86fe6e92f4
gli <you@example.com>
2025-10-30 17:06:18 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO.Pipelines;
+
+namespace Rational
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rational r1 = new Rational(2, 3);
+            r1.Display();
+            r1.Invert();
+            r1.Display();
+        }
+    }
+}
---
+++ Rational.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rational
+{
+    internal class Rational
+    {
+        public int numerator, denominator;
+        public Rational()
+        {
+            numerator = 0;
+            denominator = 1;
+        }
+        public Rational(int numerator, int denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"{this.numerator}/{this.denominator}");
+        }
+
+        public void Invert()
+        {
+            int temp = numerator;
+            numerator = denominator;
+            denominator = temp;
+        }
+    }
+}
